8d481ca6afe4ae59bff213baa9d767cd81c9b936
Kinzey R <kinzeyrahardja1@gmail.com>
2024-04-05 00:33:45 +1100

    building out typesafe querying

--- functions/utils/db.ts
+++ functions/utils/db.ts
@@ -1,7 +1,65 @@
-import { DocumentData, CollectionReference } from "firebase-admin/firestore"
+import {
+  DocumentData,
+  CollectionReference,
+  Query,
+  OrderByDirection,
+} from "firebase-admin/firestore"
 import * as admin from "firebase-admin"
 
 admin.initializeApp()
+
+// Type-safe query builder interface
+interface TypeSafeQueryBuilder<T> {
+  where<K extends keyof T>(
+    fieldPath: K,
+    opStr: FirebaseFirestore.WhereFilterOp,
+    value: T[K]
+  ): TypeSafeQueryBuilder<T>
+  orderBy<K extends keyof T>(
+    fieldPath: K,
+    directionStr?: OrderByDirection
+  ): TypeSafeQueryBuilder<T>
+  limit(limit: number): TypeSafeQueryBuilder<T>
+  get(): Promise<
+    (T & {
+      id: string
+      ref: admin.firestore.DocumentReference<T>
+    })[]
+  >
+}
+
+async function prettifyQueryData<T = DocumentData>(query: Query<T>) {
+  return (await query.get()).docs.map((doc) => {
+    const output = { ...doc.data() }
+    Object.defineProperty(output, "id", { value: doc.id, enumerable: false })
+    Object.defineProperty(output, "ref", { value: doc.ref, enumerable: false })
+    return output
+  }) as (T & { id: string; ref: admin.firestore.DocumentReference<T> })[]
+}
+
+// Implementing the type-safe query builder
+function createTypeSafeQueryBuilder<T>(
+  query: Query<T>
+): TypeSafeQueryBuilder<T> {
+  return {
+    where(fieldPath, opStr, value) {
+      return createTypeSafeQueryBuilder(
+        query.where(fieldPath as string, opStr, value)
+      )
+    },
+    orderBy(fieldPath, directionStr) {
+      return createTypeSafeQueryBuilder(
+        query.orderBy(fieldPath as string, directionStr)
+      )
+    },
+    limit(limit) {
+      return createTypeSafeQueryBuilder(query.limit(limit))
+    },
+    async get() {
+      return prettifyQueryData(query)
+    },
+  }
+}
 
 // This is just a helper to add the type to the db responses
 const createCollection = <T = DocumentData>(collectionName: string) => {
@@ -10,21 +68,13 @@ const createCollection = <T = DocumentData>(collectionName: string) => {
     .collection(collectionName) as CollectionReference<T>
 
   async function getAllDocs() {
-    return (await collectionRef.get()).docs.map((doc) => {
-      const output = { ...doc.data() }
-      // Add non-enumerable properties for the id and ref for ease of using
-      // document data
-      Object.defineProperty(output, "id", { value: doc.id, enumerable: false })
-      Object.defineProperty(output, "ref", {
-        value: doc.ref,
-        enumerable: false,
-      })
-      return output
-    }) as (T & { id: string; ref: admin.firestore.DocumentReference<T> })[]
+    return prettifyQueryData(collectionRef)
   }
 
   return {
+    collectionRef,
     getAllDocs,
+    query: () => createTypeSafeQueryBuilder(collectionRef),
   }
 }
 
@@ -33,7 +83,7 @@ type Users = {
    * to be able to query with an agreed format */
   fullNameLowercase: string
   /** Card numbers associated with the user */
-  cardQrCodes: []
+  cardQrCodes: string[]
   /** TODO: can potentially add this in the future */
   dGroup?: string
 }
