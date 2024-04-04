6691d2c760184c29a45d130dcd70675c99f3d433
Kinzey R <kinzeyrahardja1@gmail.com>
2024-04-04 23:25:49 +1100

    add test function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,9 +9,7 @@
 
 import { onRequest } from "firebase-functions/v2/https"
 import * as logger from "firebase-functions/logger"
-import * as admin from "firebase-admin"
-
-admin.initializeApp()
+import { db, admin } from "../utils/db"
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -20,6 +18,19 @@ export const helloWorld = onRequest(async (request, response) => {
   logger.info("Hello logs!", { structuredData: true })
 
   const ans = await admin.firestore().collection("attendance").get()
-  console.log("🥬", ans.docs)
+  logger.info("🥬", ans.docs)
   response.send("Hello from Firebase!")
+})
+
+export const updateUsers = onRequest(async (request, response) => {
+  // Grab all users from the user collection
+  const existingUsers = db.users.getAllDocs()
+  logger.info(existingUsers, { structuredData: true })
+
+  // Receive the array of users from the google-script.
+
+  // Decide whether we want to update any user info
+
+  // Send the output information to a firestore doc to send for the week
+  response.send("testin123")
 })
