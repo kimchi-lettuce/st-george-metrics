926d27a0501ef68453a635a6d46bbcd7a10ad849
Kinzey R <kinzeyrahardja1@gmail.com>
2024-04-04 23:26:09 +1100

    start working on db wrapper for typesafe development and users type

--- functions/utils/db.ts
+++ functions/utils/db.ts
@@ -1,24 +1,45 @@
-import {
-  DocumentData,
-  CollectionReference,
-  Firestore,
-  FieldValue,
-} from "firebase-admin/firestore"
+import { DocumentData, CollectionReference } from "firebase-admin/firestore"
 import * as admin from "firebase-admin"
+
+admin.initializeApp()
 
 // This is just a helper to add the type to the db responses
 const createCollection = <T = DocumentData>(collectionName: string) => {
-  return admin.firestore().collection(collectionName) as CollectionReference<T>
-  //   return collection(admin.firestore(), collectionName) as CollectionReference<T>
+  const collectionRef = admin
+    .firestore()
+    .collection(collectionName) as CollectionReference<T>
+
+  async function getAllDocs() {
+    return (await collectionRef.get()).docs.map((doc) => {
+      const output = { ...doc.data() }
+      // Add non-enumerable properties for the id and ref for ease of using
+      // document data
+      Object.defineProperty(output, "id", { value: doc.id, enumerable: false })
+      Object.defineProperty(output, "ref", {
+        value: doc.ref,
+        enumerable: false,
+      })
+      return output
+    }) as (T & { id: string; ref: admin.firestore.DocumentReference<T> })[]
+  }
+
+  return {
+    getAllDocs,
+  }
 }
 
 type Users = {
-  name: string
-  height: number
+  /** The user's full name in lowercase. Keeping the case consistent allows us
+   * to be able to query with an agreed format */
+  fullNameLowercase: string
+  /** Card numbers associated with the user */
+  cardQrCodes: []
+  /** TODO: can potentially add this in the future */
+  dGroup?: string
 }
 
-const usersCol = createCollection<Users>("Users")
+const db = {
+  users: createCollection<Users>("users"),
+}
 
-const ans = await usersCol.doc().update({
-  height: FieldValue.delete(),
-})
+export { db, admin }
